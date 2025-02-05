07b2fa9ff63ee6b50bc4a54ea500df66ce9b6997
Facundo Galleguillo <facundo.galleguillo@partnersly.com>
2025-02-05 15:04:54 -0300

    Update HS_SS_Customer_Statement_Pdf.js

--- HS_SS_Customer_Statement_Pdf.js
+++ HS_SS_Customer_Statement_Pdf.js
@@ -1,61 +1,70 @@
 function customerStatementPdf(type) {
 
-	var searchresults = nlapiSearchRecord('customer', 'customsearch_hs_customer_stmt_email');
+	try {
+		var searchresults = nlapiSearchRecord('customer', 'customsearch_hs_customer_stmt_email');
 
-	var resultLength = searchresults.length;
-	nlapiLogExecution('DEBUG', 'searchresults', 'Search Results (' + resultLength + '): ' + JSON.stringify(searchresults));
+		var resultLength = searchresults.length;
+		nlapiLogExecution('DEBUG', 'searchresults', 'Search Results (' + resultLength + '): ' + JSON.stringify(searchresults));
 
-	for (var i = 0; searchresults != null && i < searchresults.length; i++) {
-		var id = searchresults[i].getId();
-		//nlapiLogExecution('DEBUG', 'ID = ' , id);
+		for (var i = 0; searchresults != null && i < searchresults.length; i++) {
+			var id = searchresults[i].getId();
+			//nlapiLogExecution('DEBUG', 'ID = ' , id);
 
-		var record = nlapiLoadRecord('customer', id);
+			var record = nlapiLoadRecord('customer', id);
 
-		var customerEmail = record.getFieldValue('email');
-		var customerName = record.getFieldValue('entityid');
+			var customerEmail = record.getFieldValue('email');
+			var customerName = record.getFieldValue('entityid');
 
-		var lineCount = record.getLineItemCount('submachine');
+			var lineCount = record.getLineItemCount('submachine');
 
-		var records = new Object();
-		records['entity'] = id;
+			var records = new Object();
+			records['entity'] = id;
 
-		var todayDate = new Date();
+			var todayDate = new Date();
 
-		//var dd = String(todayDate.getDate());
-		//var mm = String(todayDate.getMonth() + 1); 
-		var yyyy = todayDate.getFullYear() - 1;
+			//var dd = String(todayDate.getDate());
+			//var mm = String(todayDate.getMonth() + 1); 
+			var yyyy = todayDate.getFullYear() - 1;
 
-		var startDate = '01/01/' + yyyy;
-		var endDate = '12/31/' + yyyy;
-		//var today = '6/1/2021';
-		//nlapiLogExecution('DEBUG', 'Today Date = ' , startDate);
+			var startDate = '01/01/' + yyyy;
+			var endDate = '12/31/' + yyyy;
+			//var today = '6/1/2021';
+			//nlapiLogExecution('DEBUG', 'Today Date = ' , startDate);
 
-		var fileArray = new Array();
+			var fileArray = new Array();
 
-		for (var z = 1; z <= lineCount; z++) {
+			for (var z = 1; z <= lineCount; z++) {
 
-			var customerSubsidiary = record.getLineItemValue('submachine', 'subsidiary', z);
-			//nlapiLogExecution('DEBUG', 'Cust Sub = ' , customerSubsidiary);
+				var customerSubsidiary = record.getLineItemValue('submachine', 'subsidiary', z);
+				//nlapiLogExecution('DEBUG', 'Cust Sub = ' , customerSubsidiary);
 
-			var sdate = new Array();
-			sdate.subsidiary = customerSubsidiary;
-			//sdate.openonly = 'T';
-			sdate.startdate = startDate;
-			sdate.statementdate = endDate;
-			sdate.formnumber = 152;
+				var sdate = new Array();
+				sdate.subsidiary = customerSubsidiary;
+				//sdate.openonly = 'T';
+				sdate.startdate = startDate;
+				sdate.statementdate = endDate;
+				sdate.formnumber = 152;
 
-			var file1 = nlapiPrintRecord('STATEMENT', id, 'PDF', sdate);
+				var file1 = nlapiPrintRecord('STATEMENT', id, 'PDF', sdate);
 
-			fileArray.push(file1);
+				fileArray.push(file1);
 
+			}
+			//nlapiLogExecution('DEBUG', 'Files ' , fileArray);
+			var emailBody = 'Hello,<br/><br/> For your reference, please find attached a statement of your account for the previous fiscal year. This statement does <b>not</b> reflect a current account balance. <br/><br/> If you have any questions regarding this statement, please email billing@hsmove.com or call (602) 889-2165. <br/><br/> Thank you for your business, we appreciate it very much!<br/>HomeSmart Accounting<br/>billing@hsmove.com<br/>(602) 889-2165';
+			nlapiSendEmail(431967, customerEmail, 'Annual Statements', emailBody, null, null, records, fileArray);
+			nlapiLogExecution('DEBUG', 'Email Sent Succesfully', 'Line: ' + i + ' | Customer: ' + id + ' | Name: ' + customerName + ' | Email: ' + customerEmail);
+
+			if (nlapiGetContext().getRemainingUsage() < 50) {
+				nlapiLogExecution('ERROR', 'LIMIT USAGE EXCEEDED', 'Script Units = ' + nlapiGetContext().getRemainingUsage());
+				nlapiYieldScript();
+			}
 		}
-		//nlapiLogExecution('DEBUG', 'Files ' , fileArray);
-		var emailBody = 'Hello,<br/><br/> For your reference, please find attached a statement of your account for the previous fiscal year. This statement does <b>not</b> reflect a current account balance. <br/><br/> If you have any questions regarding this statement, please email billing@hsmove.com or call (602) 889-2165. <br/><br/> Thank you for your business, we appreciate it very much!<br/>HomeSmart Accounting<br/>billing@hsmove.com<br/>(602) 889-2165';
-		nlapiSendEmail(431967, customerEmail, 'Annual Statements', emailBody, null, null, records, fileArray);
-		nlapiLogExecution('DEBUG', 'Email Sent Succesfully', 'Customer: ' + id + ' | Name: ' + customerName + ' | Email: ' + customerEmail);
 
-		if (nlapiGetContext().getRemainingUsage() < 50) {
-			nlapiYieldScript();
-		}
+		nlapiLogExecution('DEBUG', 'END', 'Script Execution Completed Without Errors');
+	}
+	catch(e) {
+		nlapiLogExecution('ERROR', 'Error', 'Error Message: ' + e.message + ' | Full Details: ' + JSON.stringify(e));
+		nlapiLogExecution('DEBUG', 'END', 'Script Execution Completed With Errors');
 	}
 }
